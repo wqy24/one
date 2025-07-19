8b6d5daa9f91a24a01b4c6fac099f1aba246f628
wqy24 <as2734@126.com>
2025-07-20 05:14:38 +0800

    Add news and cleans to parse.c

---
+++ parse.c
@@ -0,0 +1,108 @@
+#include "parse.h"
+#include "quickfail.h"
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+one_type new_one_type(enum one_type_base, union one_type_com);
+one_AST new_one_AST(enum one_AST_type, union one_AST_value);
+void clean_one_type(one_type);
+void clean_one_AST(one_AST);
+
+one_type new_one_type(enum one_type_base base, union one_type_com com)
+{
+  one_type res = malloc(sizeof(struct _one_type));
+  check_mem(res);
+  res->base = base;
+  res->com = com;
+  return res;
+}
+
+struct one_AST_cls *new_cls(one_AST arg, struct one_AST_cls *up)
+{
+  struct one_AST_cls *res = malloc(sizeof(struct one_AST_cls));
+  check_mem(res);
+  res->arg = arg;
+  res->up = up;
+  return res;
+}
+
+struct one_array *new_array(struct one_array *prev, one_AST curr)
+{
+  struct one_array *res = malloc(sizeof(struct one_array));
+  if (prev) prev->next = res;
+  res->next = NULL;
+  res->curr = curr;
+  return res;
+}
+
+one_AST new_one_AST(enum one_AST_type type, union one_AST_value value)
+{
+  one_AST res = malloc(sizeof(struct _one_AST));
+  check_mem(res);
+  res->type = type;
+  res->value = value;
+  return res;
+}
+
+void clean_one_type(one_type type)
+{
+  switch (type->base) {
+    case FN:
+      clean_one_type(type->com.fn.param);
+      clean_one_type(type->com.fn.res);
+      break;
+    case PAIR:
+      clean_one_type(type->com.pair.left);
+      clean_one_type(type->com.pair.right);
+      break;
+    case GEN:
+      clean_one_type(type->com.gen);
+      break;
+  }
+
+  free(type);
+}
+
+void clean_cls(struct one_AST_cls *cls) {
+  if (cls->up) clean_cls(cls->up);
+  clean_one_AST(cls->arg);
+  free(cls);
+}
+
+void clean_array(struct one_array *arr)
+{
+  clean_one_AST(arr->curr);
+  if (arr->next) clean_array(arr->next);
+  free(arr);
+}
+
+void clean_one_AST(one_AST ast)
+{
+  switch (ast.type) {
+    case LIT_STR:
+      free(ast->value.str);
+      break;
+    case LIT_REF:
+      fclose(ast->value.ref);
+      break;
+    case FN:
+      clean_one_type(ast->value.fn.param_type);
+      clean_one_AST(ast->value.fn.res);
+      clean_cls(ast->value.fn.closure);
+      break;
+    case FNCALL:
+      clean_one_AST(ast->value.fncall.fn);
+      clean_one_AST(ast->value.fncall.arg);
+      break;
+    case PAIR:
+      clean_one_AST(ast->value.pair.left);
+      clean_one_AST(ast->value.pair.right);
+      break;
+    case ARRAY:
+      clean_array(ast->value.array);
+      break;
+  }
+
+  free(ast);
+}
